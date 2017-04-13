c6d01d06d0902a9445c4bb616e5b29a77cd8d2e2
tyetao <765068439@qq.com>
2017-04-14 00:03:54 +0800

    提交代码

---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+// 添加引用
+var gulp = require('gulp');
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
+var nodemon = require('gulp-nodemon');
+
+//这个可以让express启动
+gulp.task("node", function() {
+    nodemon({
+        script: 'index.js',
+        ext: 'js',
+        ignore: 'node_modules/',
+        env: {
+            'NODE_ENV': 'development'
+        }
+    })
+});
+
+
+gulp.task('server', ["node"], function() {
+    var files = [
+        'client'
+    ];
+
+    //gulp.run(["node"]);
+    browserSync.init(files, {
+        proxy: 'http://localhost:3006',
+        browser: 'chrome',
+        notify: false,
+        open: false,
+        port: 3002
+    });
+
+    gulp.watch(files).on("change", reload);
+});
---
+++ index.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser'); //处理请求体数据
+var cookieParser = require('cookie-parser');
+
+var session = require('express-session');
+var mongoStore = require('connect-mongo')(session);//mogondb持久化ssesion
+var morgan = require('morgan'); //node.js的HTTP请求记录器中间件
+
+var app = express();
+app.use(morgan('tiny'));
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+app.use(express.static('./client'));
+
+
+// var routes = require('./server/routes/routes.js')(app);//路由API
+
+var server = app.listen(3006, function() {
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log("应用实例，访问地址为 http://%s:%s", host, port)
+});
